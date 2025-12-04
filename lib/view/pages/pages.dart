0265f965f229a96c7a9b3eeedf13ce66922a9c13
Verzional <verzional777@gmail.com>
2025-12-04 13:07:15 +0700

    Fix Home Page

--- lib/view/pages/pages.dart
+++ lib/view/pages/pages.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:uc_hub/view/pages/department/department_layout.dart';
+import '../widgets/widgets.dart';
 
 part 'home_page.dart';
 part 'login_page.dart';
