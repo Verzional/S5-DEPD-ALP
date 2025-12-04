6384377e8dcea5017a75995df58755bd831a251e
Dannprand <153193400+Dannprand@users.noreply.github.com>
2025-12-04 09:59:52 +0700

    change poppins font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:uc_hub/screens/auth/login_screen.dart';
 import 'package:uc_hub/theme/uchub_colors.dart';
 
@@ -15,32 +16,30 @@ class UCHubApp extends StatelessWidget {
       title: 'UC HUB Department',
       debugShowCheckedModeBanner: false,
       
-      // Global Theme Configuration
       theme: ThemeData(
-        fontFamily: 'Inter', // Ensure you have 'Inter' in your pubspec.yaml or remove this line
         useMaterial3: true,
-        
-        // Color Scheme derived from your Brand Colors
+
+        // APPLY POPPINS
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        primaryTextTheme: GoogleFonts.poppinsTextTheme(),
+
         colorScheme: ColorScheme.fromSeed(
           seedColor: UCHubColors.primaryEnd,
           primary: UCHubColors.primaryEnd,
           surface: UCHubColors.contentBackground,
         ),
         
-        // Default Scaffold Background
         scaffoldBackgroundColor: UCHubColors.contentBackground,
         
-        // AppBar Theme
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.white,
-          surfaceTintColor: Colors.transparent, // Removes the slight color tint on scroll
+          surfaceTintColor: Colors.transparent,
           elevation: 0,
           centerTitle: false,
         ),
       ),
       
-      // The Entry Point of the Application
-      home: const LoginScreen(), 
+      home: const LoginScreen(),
     );
   }
 }
